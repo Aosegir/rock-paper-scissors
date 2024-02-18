b16f26c734fb73ed2a398938ade41b2256a27d28
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-02-17 18:44:50 -0800

    Update 4: Completed UI changes. Game now tracks player vs. computer score and announces a winner once either reach 5 points.

--- javascript.js
+++ javascript.js
@@ -22,87 +22,58 @@ function playRound (playerSelection, computerSelection) {
     // choice is and checks it against the computer's choices, returning
     // The proper response for each of the 9 possible outcomes.
     const display = document.getElementById('display');
-    const score = document.getElementById('score');
-    let currentScore = score.textContent;
+    const playerScore = document.getElementById('player-score');
+    let currentPlayerScore = playerScore.textContent;
+    const computerScore = document.getElementById('comp-score');
+    let currentCompScore = computerScore.textContent;
     let result = document.createElement('p');
     if (playerSelection.toLowerCase() == "rock") {
-        // Player chooses rock
         if (computerSelection == "Rock") {
-            // Computer chooses rock
             result.textContent = 'You tie! Rock and rock means nobody wins!';
         } else if (computerSelection == "Paper") {
-            // Computer chooses paper
             result.textContent = "You lose! Paper beats rock!";
-            currentScore--;
+            currentCompScore++;
         } else {
-            // Computer chooses scissors
             result.textContent = "You win! Rock beats scissors!";
-            currentScore++;
+            currentPlayerScore++;
         }
     } else if (playerSelection.toLowerCase() == "paper") {
-        // Player chooses paper
         if (computerSelection == "Rock") {
-            // Computer chooses rock
             result.textContent = 'You win! Paper beats rock!';
-            currentScore++;
+            currentPlayerScore++;
         } else if (computerSelection == "Paper") {
-            // Computer chooses paper
             result.textContent = 'You tie! Paper and paper means nobody wins!';
         } else {
-            // Computer chooses scissors
             result.textContent = 'You lose! Scissors beats paper!';
-            currentScore--;
+            currentCompScore++;
         }
     } else {
-        // Player chooses scissors
         if (computerSelection == "Rock") {
-            // Computer chooses rock
             result.textContent = 'You lose! Rock beats scissors!';
-            currentScore--;
+            currentCompScore++;
         } else if (computerSelection == "Paper") {
-            // Computer chooses paper
             result.textContent = 'You win! Scissors beats paper!';
-            currentScore++;
+            currentPlayerScore++;
         } else {
-            // Computer chooses scissors
             result.textContent = 'You tie! Scissors and scissors means nobody wins!';
         }
     }
     display.appendChild(result);
-    score.textContent = currentScore;
-}
-
-// This function will simulate a 5 round game between the player and the computer.
-// Before each round, a prompt will start that will allow the player to choose
-// their move before the round starts.
-function playGame() {
-    // Tally will keep track of the overall wins vs losses to the player.
-    // A positive score will mean victory, negative means defeat, and 0 is a tie.
-    let tally = 0;
-    // For loop will run the following code 5 times.
-    for (let i = 0; i < 5; i++) {
-        // Prompt assigns the player's move to variable choice
-        let choice = prompt("Would you like to choose rock, paper, or scissors?");
-        // playRound function is invoked with the player's choice and a random
-        // computer choice.
-        tally += playRound(choice, getComputerChoice());
-    }
-
-    // If statement returns if the player won, lost, or tied based on tally result.
-    if (tally > 0) {
-        // Player has won
-        console.log("You have won! Great job!");
-    } else if (tally < 0) {
-        // Player has lost
-        console.log("You lost, but you did well! Next time, you'll win for sure!");
-    } else {
-        // Player has tied
-        console.log("You tied! Amazing! Try again, and you'll definitely win!");
+    playerScore.textContent = currentPlayerScore;
+    computerScore.textContent = currentCompScore;
+    
+    if(playerScore.textContent >= 5) {
+        const winner = document.getElementById('winner');
+        let announce = document.createElement('h1');
+        announce.textContent = "You win! Congratulations!";
+        winner.appendChild(announce);
+    } else if (computerScore.textContent >= 5) {
+        const winner = document.getElementById('winner');
+        let announce = document.createElement('h1');
+        announce.textContent = "You lost! You'll get that computer next time!";
+        winner.appendChild(announce);
     }
 }
-
-// playGame();
-
 
 /* -- DOM Manipulation -- */
 
@@ -115,3 +86,9 @@ buttons.forEach((btn) => {
         playRound(playersChoice, getComputerChoice());
     });
 });
+
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+    location.reload();
+})
